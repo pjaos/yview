48a71b941c85c1ac30e98b6869507eead2a7d038
Paul Austen <pjaos@gmail.com>
2022-02-02 06:34:24 +0000

    Bump version to 5.9

--- gui/java/src/yview/model/Constants.java
+++ gui/java/src/yview/model/Constants.java
@@ -5,7 +5,7 @@ package yview.model;
  */
 public class Constants
 {
-  public static double          VERSION                        		= 5.8;
+  public static double          VERSION                        		= 5.9;
 
   public static final String    APP_NAME                       		= "yView";
   public static final String    LOCAL_HOST					   		= "localhost";
