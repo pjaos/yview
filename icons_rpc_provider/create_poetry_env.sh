e9c31581f10f55c8db78753698fd944df325967f
Paul Austen <pjaos@gmail.com>
2026-02-11 05:56:15 +0000

    Add commented out remove poetry env cmd line for ref.

--- icons_rpc_provider/create_poetry_env.sh
+++ icons_rpc_provider/create_poetry_env.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # If you wish to remove the poetry python env uncomment this
-#pe=$(poetry env info -p)
-#if [ -n "$pe" ]; then
-#  echo "Removing $pe"
-#  rm -rf $pe
-#fi
+poetry env remove python3
 
 # Date just used to show how long the env takes to create
 date
